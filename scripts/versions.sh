05c1294b446aa6e7fa8d76e1c51abeb76c9f3fdf
celenity <celenity@celenity.dev>
2026-01-26 08:25:26 +0000

    feat: uBird 1.69.0
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,8 +3,8 @@
 
 ## uBlock Origin
 ### https://github.com/gorhill/uBlock
-### (This commit corresponds to https://github.com/gorhill/uBlock/releases/tag/1.68.0)
-UBLOCK_COMMIT="b9833670211d83e72c3a855984a3d544945de1e3"
-UBLOCK_VERSION="1.68.0"
+### (This commit corresponds to https://github.com/gorhill/uBlock/releases/tag/1.69.0)
+UBLOCK_COMMIT="976d12d048e47e58ca33cb013532c0e8b062773c"
+UBLOCK_VERSION="1.69.0"
 
 UBIRD_VERSION="${UBLOCK_VERSION}"
