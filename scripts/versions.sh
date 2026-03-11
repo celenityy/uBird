efa3c0e9f8c514e67a350075c275c101a1142fb2
celenity <celenity@celenity.dev>
2026-03-11 03:09:27 +0000

    1.70.0 - Part 1
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,9 +3,9 @@
 
 ## uBlock Origin
 ### https://github.com/gorhill/uBlock
-### (This commit corresponds to https://github.com/gorhill/uBlock/releases/tag/1.69.0)
-UBLOCK_COMMIT="976d12d048e47e58ca33cb013532c0e8b062773c"
-UBLOCK_SHA512SUM='404d0fa230cd83897dba947e9a20cd4b07122ffaf783a5150eb49646a8e8f508b39b3dfbd24ff1cda669a6c9b70b4628475a3f1356547bc8867268e6cc216552'
-UBLOCK_VERSION="1.69.0"
+### (This commit corresponds to https://github.com/gorhill/uBlock/releases/tag/1.70.0)
+UBLOCK_COMMIT="3703f5efb82dde5aaaaf590a3a37550fcd34160d"
+UBLOCK_SHA512SUM='cebc82db0c2032d3864470876ee226293f1b6b4d38b46c7c350323c5f2c98f34a5bb32bc4dd9da1d2d11209ea3d1becb430a33296d9d39a386d13783d54a99fa'
+UBLOCK_VERSION="1.70.0"
 
-UBIRD_VERSION="${UBLOCK_VERSION}.2"
+UBIRD_VERSION="${UBLOCK_VERSION}"
